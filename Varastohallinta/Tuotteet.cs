bd6f4fa82ed6ace2e5ad1d6f504f482beb3d85cb
PapersTheKingOfPapers <eetuasseri@gmail.com>
2022-08-26 12:55:15 +0300

    Initial Program Commit
    Committing before starting to code in the Program.cs file

---
+++ Varastohallinta/Tuotteet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+namespace Varastohallinta
+{
+    public class Tuotteet
+    {
+        [Key]
+        public int? id { get; set; }
+        public string? tuoteNimi { get; set; }
+        public int? tuotenHinta { get; set; }
+        public int? varastoSaldo { get; set; }
+
+    }
+}
---
+++ Varastohallinta/Varastohallinta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore; //For Database
+
+namespace Varastohallinta
+{
+    public class Varastohallinta : DbContext
+    {
+        public DbSet<Tuotteet> Tuotteet { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connection = "Data Source=.;" +
+                "Initial Catalog=Varastonhallinta;" +
+                "Integrated Security=true;" +   //"User Id=sa;" + "Password=password"
+                "MultipleActiveResultSets=true;";
+            optionsBuilder.UseSqlServer(connection);
+        }
+    }
+}
